e09420b90dd8f296f9911905ab3ea3868e4adb5a
Dasha Arakcheeva <arakcheeva-dasha@yandex.ru>
2025-02-04 18:31:58 +0300

    Commit №4

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,36 +1,47 @@
 import com.example.Feline;
 import com.example.Lion;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
+
+    @Mock
+    private Feline feline; // не знаю, излишне ли тут private
 
     @Test
     public void testGetKittens()throws Exception {
-        Feline feline = new Feline();
-        Lion lion = new Lion(feline,"Самец");
-        int actual = lion.getKittens();
+       Lion lion = new Lion(feline, "Самец");
+
+        when(feline.getKittens()).thenReturn(1);
+        int actual = lion.getKittens(); // Проверяем результат вызова метода getKittens
         int expected = 1;
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGetFood() throws Exception{
-        Feline feline = new Feline();
-        Lion lion = new Lion(feline,"Самец");
-        List<String> actual = lion.getFood();
+        Lion lion = new Lion(feline, "Самец");
+
+        when(feline.getFood("Хищник")).thenReturn(Arrays.asList("Животные", "Птицы", "Рыба"));
+
+        List<String> actual = lion.getFood(); // Проверяем результат вызова метода getFood()
         List<String> expected = Arrays.asList("Животные", "Птицы", "Рыба");
         assertEquals(expected, actual);
     }
 
     @Test
     public void checkLionException() {
-        Feline feline = new Feline();
+
         Exception exception = assertThrows(Exception.class,
                 () -> new Lion(feline, "")
         );
